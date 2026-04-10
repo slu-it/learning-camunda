d3df6598fea9c43531d53a58975e990533220e62
Stefan Ludwig <github@slu-it.de>
2026-04-10 10:32:26 +0200

    simple expenses process example

---
+++ src/main/kotlin/service/workers/ExpensesWorker.kt
@@ -0,0 +1,36 @@
+package service.workers
+
+import io.camunda.client.annotation.JobWorker
+import io.camunda.client.annotation.Variable
+import io.camunda.client.api.response.ActivatedJob
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.stereotype.Component
+import java.math.BigDecimal
+import java.util.Currency
+
+@Component
+class ExpensesWorker {
+
+    private val log = getLogger(javaClass)
+
+    @JobWorker(type = "expenses::process")
+    fun process(
+        job: ActivatedJob,
+        @Variable("employeeId") employeeId: String,
+        @Variable("amount") amount: BigDecimal,
+        @Variable("currency") currency: Currency,
+    ) {
+        log.info("Processed $amount $currency expenses for $employeeId -- ${job.processInstanceKey}")
+        return
+    }
+
+    @JobWorker(type = "expenses::informEmployee")
+    fun informEmployee(
+        job: ActivatedJob,
+        @Variable("employeeId") employeeId: String,
+        @Variable("decision") decision: String,
+    ) {
+        log.info("Informed $employeeId about the decision '$decision' -- ${job.processInstanceKey}")
+        return
+    }
+}
